a4638e8e3696bd81ca67b8063f9e9e3f5211c8da
Yigit-Yeler <yeleryigit40@gmail.com>
2024-01-20 15:02:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get("https://free-nba.p.rapidapi.com/players", {
+      params: {
+        page: '0',
+        per_page: '25'
+      },
+      headers: {
+        'X-RapidAPI-Key': '489a4fa2femshe401e0284dafe25p1959fejsn73d8f2d40328',
+        'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+      }
+    })
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((er) => {
+        console.log(er)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
